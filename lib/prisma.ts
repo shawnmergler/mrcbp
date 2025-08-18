bc0e6080861ae62043650956d0991da6fbb9aed5
shawnmergler <shawn.mergler@gmail.com>
2025-08-18 17:10:32 -0600

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,16 @@
-import { PrismaClient } from '@prisma/client'
+// lib/prisma.ts
+import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient | undefined }
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
 
 export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient({ log: ['error', 'warn'] })
+  global.prisma ??
+  new PrismaClient({
+    log: ['error', 'warn'],
+  });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+export default prisma;
